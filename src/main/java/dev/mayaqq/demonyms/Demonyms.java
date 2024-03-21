b045af11840f6456006c589eb54d391649f77b59
Nerjal Nosk <nosk.nerjal@gmx.com>
2024-03-21 16:32:17 +0100

    Central player-demonym getter method

--- src/main/java/dev/mayaqq/demonyms/Demonyms.java
+++ src/main/java/dev/mayaqq/demonyms/Demonyms.java
@@ -2,8 +2,11 @@ package dev.mayaqq.demonyms;
 
 import dev.mayaqq.demonyms.registry.DemonymsEvents;
 import dev.mayaqq.demonyms.registry.DemonymsCommands;
+import dev.mayaqq.demonyms.resources.Demonym;
 import dev.mayaqq.demonyms.resources.DemonymsProcessor;
+import dev.mayaqq.demonyms.storage.DemonymsState;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +21,10 @@ public class Demonyms implements ModInitializer {
         DemonymsProcessor.register();
         DemonymsEvents.register();
         DemonymsCommands.register();
+    }
+
+    public static Demonym getPlayerDemonym(PlayerEntity player) {
+        return DemonymsProcessor.DEMONYMS.get(DemonymsState.getPlayerState(player).demonym);
     }
 
     public static Identifier id(String path) {
